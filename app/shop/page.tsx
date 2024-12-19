141f656219fcc1b6b891410dc03a7270f608adc8
JosipSmoljko <jsmolj01@fesb.hr>
2024-12-19 23:06:27 +0100

    fixing dynamic routing

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,33 +1,41 @@
 import Link from "next/link";
+import type { Metadata } from "next";
 
-export interface Item {
+export type Photo = {
   albumId: number;
   id: number;
   title: string;
   url: string;
   thumbnailUrl: string;
-}
-
-const BASE_API_URL = "https://jsonplaceholder.typicode.com";
-
-const getPosts = async (): Promise<Item[]> => {
-  const data = await fetch(`${BASE_API_URL}/photos`);
-  return data.json();
 };
 
-export default async function Shop() {
-  const items = await getPosts();
-  return (
+
+export const metadata: Metadata = {
+  title: "Products",
+};
+
+
+export const BASE_API_URL = "https://jsonplaceholder.typicode.com";
+
+async function getPhotos(): Promise<Photo[]> {
+  const data = await fetch(`${BASE_API_URL}/photos?_limit=50`);
+  return data.json();
+}
+
+export default async function ShopPage() {
+  const photos = await getPhotos();
+
+  return  (
     <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
       <h1 className="text-3xl font-bold p-10">Shop Index Page</h1>
       <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4">
-        {items.map((item) => (
-          <div key={item.id} className="bg-white rounded-lg shadow-lg">
-            <Link href={`shop/${item.id}`}>
+        {photos.map((photo) => (
+          <div key={photo.id} className="bg-white rounded-lg shadow-lg">
+            <Link href={`shop/${photo.id}`}>
               
-                <img src={item.url} alt={item.title} className="w-full h-56 object-cover rounded-t-lg" />
+                <img src={photo.url} alt={photo.title} className="w-full h-56 object-cover rounded-t-lg" />
                 <div className="p-4">
-                  <span className="text-xl text-zinc-600 hover:underline">{item.title}</span>
+                  <span className="text-xl text-zinc-600 hover:underline">{photo.title}</span>
                 </div>
              
             </Link>
