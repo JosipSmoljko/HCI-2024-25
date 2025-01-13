8970f2cb3c9e63bb8a3f8c3c57c37c84487ef56a
JosipSmoljko <jsmolj01@fesb.hr>
2025-01-13 21:42:38 +0100

    landing page

--- app/shop/[itemId]/page.tsx
+++ app/shop/[itemId]/page.tsx
@@ -1,13 +1,9 @@
-/* <<<<<<< HEAD
-import Link from "next/link";
-=======
->>>>>>> 04c396cb04654b7e0fc58329ce3bb4927c17399b
+/* import Link from "next/link";
 import { Metadata } from "next";
 import { BASE_API_URL } from "../page";
 
 export const metadata: Metadata = {
   title: "Product Details",
-<<<<<<< HEAD
 };
 
 type PhotoPageProps = {
@@ -34,41 +30,6 @@ export default async function ShopPost({ params }: PhotoPageProps) {
   return (
     <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
       {photo && (
-=======
-};
-
-export type Photo = {
-  albumId: number;
-  id: number;
-  title: string;
-  url: string;
-  thumbnailUrl: string;
-};
-
-const getPhoto = async (id: string): Promise<Photo> => {
-  const response = await fetch(`${BASE_API_URL}/photos/${id}`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch photo");
-  }
-  return response.json();
-};
-
-// Dynamic route type
-export default async function ShopPost({ params }: { params: { itemId: string } }) {
-  const { itemId } = params;
-
-  let photo: Photo | null = null;
-
-  try {
-    photo = await getPhoto(itemId);
-  } catch (error) {
-    console.error("Error fetching photo:", error);
-  }
-
-  return (
-    <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
-      {photo ? (
->>>>>>> 04c396cb04654b7e0fc58329ce3bb4927c17399b
         <div className="flex flex-wrap">
           <div className="w-full md:w-1/2 p-4">
             <img src={photo.url} alt={photo.title} className="w-full h-auto" />
@@ -85,5 +46,4 @@ export default async function ShopPost({ params }: { params: { itemId: string } 
       )}
     </main>
   );
-}
- */
+} */
